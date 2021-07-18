8f110eff4cc590b0a3cc2ee88bab74ba27ed4fb0
KIWI_C0lliNN <raphael.manhaes@kiwime.io>
2021-07-18 16:22:06 -0300

    Refactor main

--- src/main/java/com/raphaelcollin/pricetracker/Main.java
+++ src/main/java/com/raphaelcollin/pricetracker/Main.java
@@ -1,26 +1,31 @@
 package com.raphaelcollin.pricetracker;
 
 import com.raphaelcollin.pricetracker.product.AmazonProductFetcher;
+import com.raphaelcollin.pricetracker.product.Product;
 import com.raphaelcollin.pricetracker.product.ProductFetcher;
 import com.raphaelcollin.pricetracker.utils.CustomHttpClient;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        final Collection<ProductFetcher> priceFetchers = new LinkedList<>();
-
         final CustomHttpClient httpClient = new CustomHttpClient();
 
-        priceFetchers.add(new AmazonProductFetcher(httpClient));
+        final Collection<ProductFetcher> productFetchers = new LinkedList<>();
+        productFetchers.add(new AmazonProductFetcher(httpClient));
 
-        priceFetchers
+        final Product cheapestProduct = productFetchers
                 .stream()
                 .map(fetcher -> fetcher.fetchProducts("SSD Kingston"))
                 .filter(Objects::nonNull)
                 .flatMap(Collection::stream)
-                .forEach(System.out::println);
+                .peek(System.out::println)
+                .min(Comparator.comparing(Product::getPrice))
+                .orElseThrow(() -> new RuntimeException("No products was fetched"));
+
+        System.out.println(cheapestProduct);
     }
 }
